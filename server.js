4dce18251f81bafaaeb33c51847d4ab78ce470e6
Silas Dos Santos Sousa <silas_santos@outlook.com.br>
2024-11-23 13:18:34 -0300

    Atualiza a configuração do banco de dados e ajusta o servidor

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
 import express, { json } from 'express';
-import mongo from 'mongodb'
+import conectarAoBanco from './src/config/dbConfig.js'; 
 
-//console.log(process.env.STRING_CONEXAO)
+//Conecta ao banco de dados utilizando a string de conexão fornecida por variavel de ambiente.
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id:2,
-        descricao: "Gato curioso olhando pela janela",
-        imagem: "https://placecats.com/curious/400/300"
-    },
-    {
-        id: 3,
-        descricao: "Gatinho dormindo em uma caixa",
-        imagem: "https://placecats.com/sleepybox/350/250"
-    }
-];
+// const posts = [
+//     {
+//         id: 1,
+//         descricao: "Uma foto teste",
+//         imagem: "https://placecats.com/millie/300/150"
+//     },
+//     {
+//         id:2,
+//         descricao: "Gato curioso olhando pela janela",
+//         imagem: "https://placecats.com/curious/400/300"
+//     },
+//     {
+//         id: 3,
+//         descricao: "Gatinho dormindo em uma caixa",
+//         imagem: "https://placecats.com/sleepybox/350/250"
+//     }
+// ];
 
 
 const app = express();
+//permite que o servidor interprete requisições com corpo no formato JSON
 app.use(express.json());
 
 app.listen(3000,()=> {
 console.log("Servidor escultando...");
 });
 
-app.get("/posts",(req,res)=> {
+
+
+async function getFullPosts(){
+    const db = conexao.db("imersao-istabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+
+
+
+
+app.get("/posts", async (req,res)=> {
+    const posts =  await getFullPosts()
     res.status(200).json(posts);
 });
 
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,17 +1,20 @@
 import { MongoClient } from 'mongodb';
 
+
+// export default: indica que exporta a função como padrão, permitindo seu uso em outros módulos ou arquivos.
+
 export default async function conectarAoBanco(stringConexao) {
-  let mongoClient;
-
-  try {
-      mongoClient = new MongoClient(stringConexao);
-      console.log('Conectando ao cluster do banco de dados...');
-      await mongoClient.connect();
-      console.log('Conectado ao MongoDB Atlas com sucesso!');
-
-      return mongoClient;
-  } catch (erro) {
-      console.error('Falha na conexão com o banco!', erro);
-      process.exit();
+    let mongoClient;
+  
+    try {
+        mongoClient = new MongoClient(stringConexao);
+        console.log('Conectando ao cluster do banco de dados... \n Tentativa de conexão em: '+ new Date().toLocaleString());
+        await mongoClient.connect();
+        console.log('Conectado ao MongoDB Atlas com sucesso!\n DataHora da Conexão: '+ new Date().toLocaleString());
+  
+        return mongoClient;
+    } catch (erro) {
+        console.error('Falha na conexão com o banco!', erro);
+        process.exit();
+    }
   }
-}
